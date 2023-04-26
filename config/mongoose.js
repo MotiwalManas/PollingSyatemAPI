3edbda228d1b1274c15ffc3b1522736799d9511b
Manas <123235470+MotiwalManas@users.noreply.github.com>
2023-04-26 12:54:38 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,25 +3,12 @@ mongoose.connect(
   "mongodb+srv://rishikesh:rishikesh@cluster1.behm7nv.mongodb.net/?retryWrites=true&w=majority"
 );
 
-// ----->this the code for using the cloud mongodb atlas -------------------------------------------------------
-// const url='mongodb+srv://GarimaJain:kPKjUaqgs0b1ApUZ@cluster0.whteafy.mongodb.net/?retryWrites=true&w=majority';
+
 const connectParams = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-// mongoose
-//     .connect(url,connectParams)
-//     .then(()=>{
-//         console.info("yeah successfully connected to mongodb")
-//     })
-//     .catch((err)=>{
-//         console.log('error in mongodb',err);
-//     })
-
-// ------------------------------------------------------------------------------------
-
-// ----->this the code for manually using the mongodb of local system
 
 const db = mongoose.connection;
 
